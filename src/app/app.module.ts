2783c6b2b505169e465a162060d97942e0d9c4e5
PalmTreeBlvd <55242169+PalmTreeBlvd@users.noreply.github.com>
2020-09-21 11:14:05 -0400

    Created folder structure for all components. Moved existing components to their new folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TweetsComponent } from './components/tweets/tweets.component';
-import { TweetComponent } from './components/tweet/tweet.component';
-import { ColorThemeToggleComponent } from './components/color-theme-toggle/color-theme-toggle.component';
+import { TweetsComponent } from './components/main-feed/tweets/tweets.component';
+import { TweetComponent } from './components/main-feed/tweet/tweet.component';
+import { ColorThemeToggleComponent } from './components/side-bar/color-theme-toggle/color-theme-toggle.component';
 
 @NgModule({
   declarations: [AppComponent, TweetsComponent, TweetComponent, ColorThemeToggleComponent],
