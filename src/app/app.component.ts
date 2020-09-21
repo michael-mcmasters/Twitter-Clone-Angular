392cc998a9a17d287085b25c905d7e9b4304e3f8
PalmTreeBlvd <55242169+PalmTreeBlvd@users.noreply.github.com>
2020-09-21 09:33:08 -0400

    background color of entire page is now set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+
+  // This allows us to change background color of entire web page in this component's css.
+  // You would think doing * {} in styles.css would work, but because that is outside of the <body> element, it would not be able to access our dark/light color themes.
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'learning-angular-project';
