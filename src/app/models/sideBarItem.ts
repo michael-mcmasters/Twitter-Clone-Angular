90085dd716e72c8b8d9357366889d345d815bdec
PalmTreeBlvd <55242169+PalmTreeBlvd@users.noreply.github.com>
2020-10-11 05:27:34 -0400

    Revert "Delete sideBarItem.CSSClass and will use sideBarItem.name instead"
    This reverts commit bc079cc2ab9837c9efa14bc28fe2749c39643070.
    By using sideBarElement.CSSClass, it is more obvious which objects actually have a CSS class for their name.
    Since there are only two objects that do, it makes more sense to make that obvious with empty strings for the objects that don't have a CSS class name.

--- src/app/models/sideBarItem.ts
+++ src/app/models/sideBarItem.ts
@@ -1,4 +1,5 @@
 export interface SideBarItem {
   name: string;
   iconName: string; // The name of the Material Theme icon. Icons found here: https://material.io/resources/icons/?icon=more_horiz&style=baseline
+  CSSClass: string;
 }
