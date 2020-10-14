c10c3010ee3169705946ad457c1ad8a2474a7990
PalmTreeBlvd <55242169+PalmTreeBlvd@users.noreply.github.com>
2020-10-14 12:03:38 -0400

    Set max width for edge case, really long word overflowing div size

--- src/app/components/tweet-composer/tweet-composer/tweet-composer.component.ts
+++ src/app/components/tweet-composer/tweet-composer/tweet-composer.component.ts
@@ -7,32 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TweetComposerComponent implements OnInit {
   avatarImage: string = './assets/images/jm-avatar.jpg';
-
-  clickedOnInput: boolean = false;
-  charactorCount: number = 0;
-  charCount = document.getElementsByClassName('edit-box')[0];
-
+  
   constructor() {}
-
-  ngOnInit(): void {}
-
-  do() {
-    if (this.charCount) {
-      this.charactorCount = this.charCount.innerText.length;
-    }
-
-    if (charactorCount > 0 && clickedOnInput) {
-      document.getElementById('MyConteditableElement').classList.add('holder');
-    }
-
-    if (charactorCount == 0 && !clickedOnInput) {
-      document
-        .getElementById('MyConteditableElement')
-        .classList.remove('holder');
-    }
-  }
-
-  getContent(innerText) {
-    this.clickedOnInput = false;
-  }
+  
+  ngOnInit() {}
 }
