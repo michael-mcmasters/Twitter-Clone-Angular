bb26913b7c4d341d00572619b47a75ea1b44ddb3
PalmTreeBlvd <55242169+PalmTreeBlvd@users.noreply.github.com>
2020-10-14 14:24:51 -0400

    Change tweet icons from bold to outline

--- src/app/components/main-feed/tweet/tweet.component.ts
+++ src/app/components/main-feed/tweet/tweet.component.ts
@@ -1,22 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Tweet } from 'src/app/models/tweet';
-import { faComment, faRetweet, faHeart, faShare } from '@fortawesome/free-solid-svg-icons'
+import {
+  faRetweet,
+  faExternalLinkAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-tweet',
   templateUrl: './tweet.component.html',
-  styleUrls: ['./tweet.component.css']
+  styleUrls: ['./tweet.component.css'],
 })
 export class TweetComponent implements OnInit {
   @Input() tweet: Tweet;
   faRetweet = faRetweet;
   faHeart = faHeart;
   faComment = faComment;
-  faShare = faShare;
+  faExternalLinkAlt = faExternalLinkAlt;
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
